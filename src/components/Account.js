21be324beec7a4b553225564b8b0f573322b6af0
cameeoHub <jacobwkendall@outlook.com>
2022-08-16 16:59:05 +0100

    div naming and basic html

--- src/components/Account.js
+++ src/components/Account.js
@@ -9,6 +9,10 @@ const Account = () => {
   return (
     <div>
         <h1>Temp message for Account page</h1>
+        <p>Username</p>
+        <p>Full Name</p>
+        <p>Password</p>
+        <button>Create New Password</button>
     </div>
   )  
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,10 @@ import {useState, useEffect} from "react";
 
 const Home = () => {
 
-
-
-
-
     return (
-        <div>
-            <h1>Temp Homepage message</h1>
+        <div className="homeBlock">
+            <h1>Welcome to ClotheNation</h1>
+            <p>Please Sign-Up or Login to browse the store</p>
         </div>
     )
 }
