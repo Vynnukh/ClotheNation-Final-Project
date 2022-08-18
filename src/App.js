e80cc480b2103dbb65cadf2978b57c1380485fe7
Vynnukh <bwhittingham213@gmail.com>
2022-08-18 12:10:10 +0100

    Members Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home.js"
 import Account from "./components/Account.js"
 import Marketplace from "./components/Marketplace.js"
 import LoginSignUpModal from "./components/Modals/LoginSignUpModal.js"
+import Members from "./components/Members.js"
+import LogoutModal from "./components/Modals/LogoutModal.js";
 import { useState } from 'react';
 import { Header1 } from './styles/Header.style'
 import './App.css';
@@ -13,10 +15,10 @@ import { H1 } from './styles/Text.style';
 
 
 
-function App(props) {
+function App() {
 
   const [show, setShow] = useState(false)
-  // const [login, setLogin] = useState([])
+  const [showLogout, setShowLogout] = useState(false)
   const [loggedIn, setLoggedIn] = useState(false)
 
 
@@ -45,20 +47,24 @@ const GlobalStyle = createGlobalStyle`
           <Navlinks>
             <Li1><Link to="/">Home</Link></Li1>
             <Li1><Link to="/Account">Account</Link></Li1>
+            <Li1><Link to="/Members">Members</Link></Li1>
             <Li1><Link to="/Marketplace">Marketplace</Link></Li1>
           </Navlinks>
-          <a href="#" id='open-button' onClick={() => setShow(true)}><Button>LogIn</Button></a>
+          <a href="#" onClick={() => setShow(true)}><Button>LogIn</Button></a>
+          
+          <a href="#" onClick={() => setShowLogout(true)}><Button>Log out</Button></a>
+
         </Header1>
-        <div>
-          <button id='open-button' onClick={() => setShow(true)}>Login/SignUp</button>
-        </div>
+        
+        <LogoutModal title = "Logout" onClose = {() => setShowLogout(false)} showLogout = {showLogout} loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>
 
         <LoginSignUpModal title = "Login/SignUp" onClose = {() => setShow(false)} show = {show} loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>
       </nav>
       <Routes>
         <Route path='/' element = {<Home loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>}></Route>
-        <Route path='/Account' element = {<Account/>}></Route>
-        <Route path='/Marketplace' element = {<Marketplace/>}></Route>
+        <Route path='/Account' element = {<Account loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>}></Route>
+        <Route path="/Members" element = {<Members loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>}></Route>
+        <Route path='/Marketplace' element = {<Marketplace loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>}></Route>
       </Routes>
       </BrowserRouter>
    
---
+++ src/components/Members.js
@@ -0,0 +1,13 @@
+
+
+const Members = () => {
+   
+    
+    return (
+        <div>
+            <h1>Temp Message for memebrs Page</h1>
+        </div>
+    )
+}
+
+export default Members
