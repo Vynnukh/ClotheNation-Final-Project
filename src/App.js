26f5ce5238384c4b0bccdea6594a3eb4293ee61d
steve pearson <stevepearsontec@yahoo.com>
2022-08-19 10:29:44 +0100

    navbarchanges

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import styled from 'styled-components'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from "./components/Home.js"
 import Account from "./components/Account.js"
@@ -7,10 +8,25 @@ import LoginSignUpModal from "./components/Modals/LoginSignUpModal.js"
 import { useState } from 'react';
 import { Header1 } from './styles/Header.style'
 import './App.css';
-import { Navlinks, Li1 } from './styles/Navlinks.style.js';
+import { Navlinks, Li1, NavBar1, LinkStyle } from './styles/Navlinks.style.js';
 import { Button } from './styles/Button.style';
-import { H1 } from './styles/Text.style';
-
+import { H1, P1 } from './styles/Text.style';
+import { Divhome, Divlogo, Divpic } from './styles/Divhome.style';
+import c1 from './logo/c1.png';
+import l from './logo/l.png';
+import o from './logo/o.png';
+import t from './logo/t.png';
+import h from './logo/h.png';
+import e from './logo/e.png';
+import mod from './logo/mod.jpg';
+import n from './logo/n.png';
+import a from './logo/a.png';
+import t2 from './logo/t.png';
+import i from './logo/i.png';
+import o2 from './logo/o.png';
+import n2 from './logo/n.png';
+import photo2 from './logo/photo2.jpg';
+import photo from './logo/photo.jpg';
 
 
 function App(props) {
@@ -22,14 +38,11 @@ function App(props) {
 
 const GlobalStyle = createGlobalStyle`
     body {
-        font-family: 'Monofett', cursive;
-        font-family: 'Inconsolata', monospace;
+      
     }
 `;
 
-
-
-  return (
+return (
   
     <div className="App">
 
@@ -37,34 +50,50 @@ const GlobalStyle = createGlobalStyle`
       
 
       <BrowserRouter>
-      <nav className="navBar">
+      <NavBar1>
         <Header1>
-          <div>
-            <H1>ClotheNation</H1>
-          </div>
+          <Divlogo>
+          <img src={c1} className="App-logoc1" alt="logo" />
+          <img src={l} className="App-logol" alt="logo" />
+          <img src={o} className="App-logoo" alt="logo" />
+        <img src={t} className="App-logot" alt="logo" />
+        <img src={h} className="App-logoh" alt="logo" />
+        <img src={e} className="App-logoe" alt="logo" />
+        <img src={mod} className="App-logomod" alt="logo" />
+        <img src={n} className="App-logon" alt="logo" />
+        <img src={a} className="App-logoa" alt="logo" />
+        <img src={t2} className="App-logot2" alt="logo" />
+        <img src={i} className="App-logoi" alt="logo" />
+        <img src={o2} className="App-logoo2" alt="logo" />
+        <img src={n2} className="App-logon2" alt="logo" />
+          </Divlogo>
           <Navlinks>
-            <Li1><Link to="/">Home</Link></Li1>
-            <Li1><Link to="/Account">Account</Link></Li1>
-            <Li1><Link to="/Marketplace">Marketplace</Link></Li1>
+            <Li1><Link to="/"><a>Home</a></Link></Li1>
+            <Li1><Link to="/Account"><a>Account</a></Link></Li1>
+            <Li1><Link to="/Marketplace"><a>Marketplace</a></Link></Li1>
+            
           </Navlinks>
-          <a href="#" id='open-button' onClick={() => setShow(true)}><Button>LogIn</Button></a>
+          <a href="#" id='open-button' onClick={() => setShow(true)}><Button><a>LogIn</a></Button></a>
         </Header1>
-        <div>
-          <button id='open-button' onClick={() => setShow(true)}>Login/SignUp</button>
-        </div>
+        
+        
 
         <LoginSignUpModal title = "Login/SignUp" onClose = {() => setShow(false)} show = {show} loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>
-      </nav>
+      </NavBar1>
       <Routes>
         <Route path='/' element = {<Home loggedIn = {loggedIn} setLoggedIn = {setLoggedIn}/>}></Route>
         <Route path='/Account' element = {<Account/>}></Route>
         <Route path='/Marketplace' element = {<Marketplace/>}></Route>
       </Routes>
       </BrowserRouter>
-   
+      <Divpic>
+      <img src= {photo2} alt="girls2"/>
+      <img src= {photo} alt="girls"/>
+    </Divpic>
     </div>
     
   );
 }
 
 export default App;
+
