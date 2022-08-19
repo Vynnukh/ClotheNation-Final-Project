698ccafa91bc39689d4ba5d02e5eeb83e4622f14
Kradnar <pete.daniel.1975@gmail.com>
2022-08-19 16:04:31 +0100

    CRUD 1st update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,25 @@
 export const signUp = async (username, password, email, setUser, setLoggedIn) => {
     try {
-        const response = await fetch("http://localhost:5001/user",
-        {method: "POST",
-        headers: {"Content-Type": "application/json"},
-    body: JSON.stringify
-    ({"Username": username,
-    "Password": password,
-        "Email": email
-    })
-}
-)
-const data = await response.json()
-setUser()
-setLoggedIn(true)
-
-    } catch(error) {
+        const response = await fetch("https://clothenation.herokuapp.com/user",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                "Username": username,
+                "Password": password,
+                "Email": email
+            })
+        })
+        const data = await response.json()
+        setUser()
+        setLoggedIn(true)
+    } 
+    catch(error) {
         console.log(error)
     }
 }
-
 export const login = async (username, password, setUser, setLoggedIn) => {
     try {
-        const response = await fetch("http://localhost:5001/login", {
+        const response = await fetch("https://clothenation.herokuapp.com/login", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
@@ -32,7 +30,62 @@ export const login = async (username, password, setUser, setLoggedIn) => {
         const data = await response.json()
         setUser()
         setLoggedIn(true)
-    } catch(error) {
+    } 
+    catch(error) {
         console.log(error)
+    }
+}
+export const getAllUsers = async (setList) => {
+    try {
+        const response = await fetch("https://clothenation.herokuapp.com/user", {
+            // headers: {"Content-Type": "application/json"},
+            method: "GET",
+        });
+        const data = await response.json()
+        setList(data.allUsers)
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+export const updateU = async (username, password, newUsername, newEmail, newPassword, setter) => {
+    try {
+    console.log("updateU function called")
+        const response = await fetch("https://clothenation.herokuapp.com/user", {
+            headers: {"Content-Type": "application/json"},
+            method: "PATCH",
+            body: JSON.stringify({
+                "username" : username,
+                "password" : password,
+                "newUsername" : newUsername,
+                "newEmail" : newEmail,
+                "newPassword" : newPassword
+            })
+        });
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        console.log("updateU function ended")
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+export const deleteU = async (username, password, setter) => {
+    try {
+        const response = await fetch("https://clothenation.herokuapp.com/user", {
+            headers: {"Content-Type": "application/json"},
+            method: "DELETE",
+            body: JSON.stringify({
+                "username" : username,
+                "password" : password
+            })
+        });
+        const data = await response.json();
+        console.log(data.user);
+        console.log("User Deleted")
+    } 
+    catch (error) {
+        console.log(error);
     }
 }
