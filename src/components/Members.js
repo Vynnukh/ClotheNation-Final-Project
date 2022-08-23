e2d39d0ef4a731feb4e69795f72a492308a0cf6a
Kradnar <pete.daniel.1975@gmail.com>
2022-08-23 15:04:00 +0100

    Members.js cleaned up

--- src/components/Members.js
+++ src/components/Members.js
@@ -3,35 +3,30 @@ import {useState,useEffect} from 'react';
 
 const Members = ({loggedIn}) => {
 const [displayUsers, setUserDisp] = useState(false);
-
 const [userList, setUserList] = useState([]);
 
 useEffect(()=> {
     getAllUsers(setUserList);
 },[])
-
     return (
         <div>
-        <button onClick={(event) => setUserDisp(!displayUsers)}>Toggle Users</button>
-                    {displayUsers &&
-        userList.map((item,index) => {
-          return (
+            {loggedIn ?
             <div>
-            <h2>{item}</h2>
-            </div>
-          )
-        })}
-
-
-
-        {loggedIn ?
-        <div>
-            <h1>Temp Message for members Page</h1>
+            <h1>Welcome to Members Page</h1>
+            <button onClick={(event) => setUserDisp(!displayUsers)}>Toggle Users</button>
+                {displayUsers && userList.map((item,index) => {
+                return (
+                        <div>
+                        <h2>{item}</h2>
+                        </div>
+                        )
+                    }
+                )}  
+            </div> : 
+            <div>
+                <h2>You must be logged in to view members</h2>
+            </div>}
         </div>
-    : <div>
-        <h2>You must be logged in to view members</h2>
-    </div>}
-    </div>
     )
 }
 
